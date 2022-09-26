50e38ef2ae0e6bb558f6b9ffdd1fd846d7d53c61
BlackStone5677 <65448536+BlackStone5677@users.noreply.github.com>
2022-09-27 02:31:01 +0330

    [E6E] halved craft time of infused sunmetal blocks
    halved crafting time of infused sunmetal blocks so it's more efficient to craft blocks at a time instead of ingots, did the same for tainted gold

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/altar.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/altar.js
@@ -19,7 +19,7 @@ onEvent('recipes', (event) => {
             output: { item: 'naturesaura:infused_iron_block' },
             aura_type: 'naturesaura:overworld',
             aura: 135000,
-            time: 700,
+            time: 350,
             id: 'naturesaura:altar/infused_iron_block'
         },
         {
@@ -35,7 +35,7 @@ onEvent('recipes', (event) => {
             output: { item: 'naturesaura:tainted_gold_block' },
             aura_type: 'naturesaura:nether',
             aura: 135000,
-            time: 700,
+            time: 350,
             id: 'naturesaura:altar/tainted_gold_block'
         },
         {
